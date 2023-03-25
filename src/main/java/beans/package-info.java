61ed61a5cf43422b3f7f3d43995f489aba59da90
Louman127 <Louman127@gmx.com>
2023-03-25 13:46:57 -0400

    added mvc structure and jsps

---
+++ src/main/java/beans/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author L
+ *
+ */
+package beans;
---
+++ src/main/java/ehotel/EhotelServlet.java
@@ -0,0 +1,10 @@
+package ehotel;
+
+public class EhotelServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
